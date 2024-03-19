be0adc7e5ab9e8daa7405ecd7a2726985071773a
Andreas Reichel <andreas@manticore-projects.com>
2024-03-19 09:08:35 +0700

    style: improve the function rewrite
    Signed-off-by: Andreas Reichel <andreas@manticore-projects.com>

--- src/main/java/com/manticore/transpiler/ExpressionTranspiler.java
+++ src/main/java/com/manticore/transpiler/ExpressionTranspiler.java
@@ -20,6 +20,7 @@ package com.manticore.transpiler;
 import net.sf.jsqlparser.expression.CastExpression;
 import net.sf.jsqlparser.expression.DateTimeLiteralExpression;
 import net.sf.jsqlparser.expression.Expression;
+import net.sf.jsqlparser.expression.ExtractExpression;
 import net.sf.jsqlparser.expression.Function;
 import net.sf.jsqlparser.expression.IntervalExpression;
 import net.sf.jsqlparser.expression.StringValue;
@@ -36,6 +37,81 @@ import java.util.regex.Pattern;
  * The type Expression transpiler.
  */
 public class ExpressionTranspiler extends ExpressionDeParser {
+  enum DateFunction {
+   CURRENT_DATE
+    , DATE
+    , DATE_ADD
+    , DATE_DIFF
+    , DATE_SUB
+    , DATE_TRUNC
+    , EXTRACT
+    , FORMAT_DATE;
+
+    public static ExpressionTranspiler.DateFunction from(String name) {
+      ExpressionTranspiler.DateFunction function = null;
+      try {
+        function = Enum.valueOf(ExpressionTranspiler.DateFunction.class, name.toUpperCase());
+      } catch (Exception ignore) {
+
+      }
+      return function;
+    }
+
+    public static ExpressionTranspiler.DateFunction from(Function f) {
+      return from(f.getName());
+    }
+
+  }
+  public static boolean isDateFunction(Function f) {
+    return DateFunction.from(f.getName())!=null;
+  }
+
+  enum TranspiledFunction {
+    CURRENT_DATE
+    , DATE
+    , DATE_ADD
+    , DATE_DIFF
+    , DATE_SUB
+    , DATE_TRUNC
+    , EXTRACT
+    , FORMAT_DATE
+
+    , NVL;
+
+    public static TranspiledFunction from(String name) {
+      TranspiledFunction function = null;
+      try {
+        function = Enum.valueOf(TranspiledFunction.class, name.toUpperCase());
+      } catch (Exception ignore) {
+
+      }
+      return function;
+    }
+
+    public static TranspiledFunction from(Function f) {
+      return from(f.getName());
+    }
+  }
+
+  enum UnsupportedFunction {
+    DATE_FROM_UNIX_DATE
+    ;
+
+    public static UnsupportedFunction from(String name) {
+      UnsupportedFunction function = null;
+      try {
+        function = Enum.valueOf(UnsupportedFunction.class, name.toUpperCase());
+      } catch (Exception ignore) {
+
+      }
+      return function;
+    }
+
+    public static UnsupportedFunction from(Function f) {
+      return from(f.getName());
+    }
+  }
+
   private final JSQLTranspiler.Dialect inputDialect;
 
   public ExpressionTranspiler(SelectVisitor selectVisitor, StringBuilder buffer,
@@ -270,190 +346,207 @@ public class ExpressionTranspiler extends ExpressionDeParser {
     }
   }
 
-
-
   @SuppressWarnings({"PMD.CyclomaticComplexity"})
   public void visit(Function function) {
+    if (UnsupportedFunction.from(function)!=null) {
+      throw new RuntimeException("Unsupported: " + function.getName() + " is not supported by DuckDB (yet).");
+    }
+
+    Expression rewrittenExpression = null;
     ExpressionList<?> parameters = function.getParameters();
-
-    // @todo: figure out a better rewrite mechanism
-    if (function.getName().equalsIgnoreCase("nvl")) {
-      function.setName("Coalesce");
-      super.visit(function);
-    } else if (function.getName().equalsIgnoreCase("date")) {
-
-      final CastExpression expression;
-      switch (parameters.size()) {
-        // DATE(DATETIME '2016-12-25 23:59:59') AS date_dt
-        case 1:
-          // DATE(TIMESTAMP '2016-12-25 05:30:00+07', 'America/Los_Angeles') AS date_tstz
-        case 2:
-          expression = new CastExpression("Cast").withLeftExpression(parameters.get(0))
-              .withType(new ColDataType().withDataType("DATE"));
-          super.visit(expression);
-          break;
-        case 3:
-          function.setName("MAKE_DATE");
-          super.visit(function);
-          break;
+    TranspiledFunction f = TranspiledFunction.from(function.getName());
+    if (f!=null) {
+      switch (f) {
+          case CURRENT_DATE:
+            rewriteCurrentDateFunction(parameters);
+            break;
+          case DATE:
+            rewrittenExpression = rewriteDateFunction(function, parameters);
+            break;
+          case DATE_ADD:
+            rewriteDateAddFunction(parameters);
+            break;
+          case DATE_DIFF:
+            rewriteDateDiffFunction(function, parameters);
+            break;
+          case DATE_SUB:
+            // Google BigQuery DATE_SUB means Subtract interval from Date
+            // shall be translated to DATE_ADD
+            rewriteDateSubFunction(function, parameters);
+            break;
+          case DATE_TRUNC:
+            rewriteDateTruncFunction(function, parameters);
+            break;
+          case EXTRACT:
+            // Extract is a specific `ExtractExpression`
+            break;
+          case FORMAT_DATE:
+            rewriteDateFormatFunction(function, parameters);
+            break;
+          case NVL:
+            function.setName("Coalesce");
+            break;
       }
-    } else if (function.getName().equalsIgnoreCase("date_diff")
-        && inputDialect == JSQLTranspiler.Dialect.GOOGLE_BIG_QUERY) {
-      switch (parameters.size()) {
-        case 3:
-          ExpressionList<Expression> reversedParameters = new ExpressionList<>();
-          // Date Part "ISOWEEK" exists and is not supported on DuckDB
-          if (parameterWEEK(parameters, 2)) {
-            reversedParameters.add(new StringValue("WEEK"));
-            buffer.append(" /*APPROXIMATION: WEEK*/ ");
-          } else if (parameters.get(2) instanceof Column && ((Column) parameters.get(2)).toString()
-              .replaceAll(" ", "").equalsIgnoreCase("ISOWEEK")) {
-            reversedParameters.add(new StringValue("WEEK"));
-          } else {
-            // translate DAY into String 'DAY'
-            reversedParameters.add(!(parameters.get(2) instanceof StringValue)
-                ? new StringValue(parameters.get(2).toString())
-                : parameters.get(2));
-          }
-
-          // enforce DATE casting
-          reversedParameters.add(parameters.get(1) instanceof StringValue
-              ? new DateTimeLiteralExpression().withType(DateTimeLiteralExpression.DateTime.DATE)
-                  .withValue(((StringValue) parameters.get(1)).toString())
-              : parameters.get(1));
-
-          // enforce DATE casting
-          reversedParameters.add(parameters.get(0) instanceof StringValue
-              ? new DateTimeLiteralExpression().withType(DateTimeLiteralExpression.DateTime.DATE)
-                  .withValue(((StringValue) parameters.get(0)).toString())
-              : parameters.get(0));
-          function.setParameters(reversedParameters);
-        default:
-          super.visit(function);
-      }
-    } else if (function.getName().equalsIgnoreCase("DATE_FROM_UNIX_DATE") && parameters.size() == 1
-        && inputDialect == JSQLTranspiler.Dialect.GOOGLE_BIG_QUERY) {
-
-      throw new UnsupportedOperationException(
-          "Function DATE_FROM_UNIX_DATE(long) is not supported in DuckDB.");
-
-    } else if (function.getName().equalsIgnoreCase("date_add")
-        && inputDialect == JSQLTranspiler.Dialect.GOOGLE_BIG_QUERY) {
-      switch (parameters.size()) {
-        case 2:
-          if (parameters.get(1) instanceof IntervalExpression) {
-            IntervalExpression interval = (IntervalExpression) parameters.get(1);
-            interval.setExpression(
-                new StringValue(interval.getParameter() + " " + interval.getIntervalType()));
-            interval.setIntervalType("");
-          }
-        default:
-          super.visit(function);
-      }
-    } else if (function.getName().equalsIgnoreCase("date_sub")
-        && inputDialect == JSQLTranspiler.Dialect.GOOGLE_BIG_QUERY) {
-      // Google BigQuery DATE_SUB means Subtract interval from Date
-      // shall be translated to DATE_ADD
-      switch (parameters.size()) {
-        case 2:
-          if (parameters.get(1) instanceof IntervalExpression) {
-            IntervalExpression interval = (IntervalExpression) parameters.get(1);
-            String negatedParameter =
-                interval.getParameter().startsWith("-") ? interval.getParameter().substring(1)
-                    : "-" + interval.getParameter();
-            interval.setExpression(
-                new StringValue(negatedParameter + " " + interval.getIntervalType()));
-            interval.setIntervalType("");
-            function.setName("DATE_ADD");
-          }
-        default:
-          super.visit(function);
-      }
-    } else if (function.getName().equalsIgnoreCase("date_trunc")
-        && inputDialect == JSQLTranspiler.Dialect.GOOGLE_BIG_QUERY) {
-      // Google BigQuery DATE_SUB means Subtract interval from Date
-      // shall be translated to DATE_ADD
-      switch (parameters.size()) {
-        case 2:
-          ExpressionList<Expression> reversedParameters = new ExpressionList<>();
-
-          // Date Part "ISOWEEK" exists and is not supported on DuckDB
-          if (parameterWEEK(parameters, 1)) {
-            reversedParameters.add(new StringValue("WEEK"));
-            buffer.append(" /*APPROXIMATION: WEEK*/ ");
-          } else if (parameters.get(1) instanceof Column && ((Column) parameters.get(1)).toString()
-              .replaceAll(" ", "").equalsIgnoreCase("ISOWEEK")) {
-            reversedParameters.add(new StringValue("WEEK"));
-          } else {
-            // translate DAY into String 'DAY'
-            reversedParameters.add(!(parameters.get(1) instanceof StringValue)
-                ? new StringValue(parameters.get(1).toString())
-                : parameters.get(1));
-          }
-
-          // enforce DATE casting
-          reversedParameters.add(parameters.get(0) instanceof StringValue
-              ? new DateTimeLiteralExpression().withType(DateTimeLiteralExpression.DateTime.DATE)
-                  .withValue(((StringValue) parameters.get(0)).toString())
-              : parameters.get(0));
-
-          function.setParameters(reversedParameters);
-        default:
-          super.visit(function);
-      }
-    } else if (function.getName().equalsIgnoreCase("extract")
-        && inputDialect == JSQLTranspiler.Dialect.GOOGLE_BIG_QUERY) {
-      // Google BigQuery DATE_SUB means Subtract interval from Date
-      // shall be translated to DATE_ADD
-      switch (parameters.size()) {
-        case 2:
-          ExpressionList<Expression> newParameters = new ExpressionList<>();
-
-          // enforce DATE casting
-          newParameters.add(parameters.get(1) instanceof StringValue
-              ? new DateTimeLiteralExpression().withType(DateTimeLiteralExpression.DateTime.DATE)
-                  .withValue(((StringValue) parameters.get(1)).toString())
-              : parameters.get(1));
-
-          // Date Part "ISOWEEK" exists and is not supported on DuckDB
-          if (parameterWEEK(parameters, 0)) {
-            newParameters.add(new StringValue("WEEK"));
-            buffer.append(" /*APPROXIMATION: WEEK*/ ");
-          } else if (parameters.get(0) instanceof Column && ((Column) parameters.get(0)).toString()
-              .replaceAll(" ", "").equalsIgnoreCase("ISOWEEK")) {
-            newParameters.add(new StringValue("WEEK"));
-          } else {
-            // translate DAY into String 'DAY'
-            newParameters.add(!(parameters.get(0) instanceof StringValue)
-                ? new StringValue(parameters.get(0).toString())
-                : parameters.get(0));
-          }
-          function.setParameters(newParameters);
-        default:
-          super.visit(function);
-      }
-    } else if (function.getName().equalsIgnoreCase("format_date")
-        && inputDialect == JSQLTranspiler.Dialect.GOOGLE_BIG_QUERY) {
-      ExpressionList<Expression> reversedParameters = new ExpressionList<>();
-      switch (parameters.size()) {
-        case 2:
-          // enforce DATE casting
-          reversedParameters.add(parameters.get(1) instanceof StringValue
-              ? new DateTimeLiteralExpression().withType(DateTimeLiteralExpression.DateTime.DATE)
-                  .withValue(((StringValue) parameters.get(1)).toString())
-              : parameters.get(1));
-
-          // pass through the format parameter string
-          // @todo: parse and replace those parameters where necessary
-          reversedParameters.add(parameters.get(0));
-
-          function.setName("StrfTime");
-          function.setParameters(reversedParameters);
-        default:
-          super.visit(function);
-      }
-    } else {
+    }
+    if (rewrittenExpression==null) {
       super.visit(function);
     }
+  }
+
+  private static void rewriteDateFormatFunction(Function function, ExpressionList<?> parameters) {
+    ExpressionList<Expression> reversedParameters = new ExpressionList<>();
+    switch (parameters.size()) {
+      case 2:
+        // enforce DATE casting
+        reversedParameters.add(parameters.get(1) instanceof StringValue
+                               ? new DateTimeLiteralExpression().withType(DateTimeLiteralExpression.DateTime.DATE)
+                                       .withValue(parameters.get(1).toString())
+                               :parameters.get(1));
+
+        // pass through the format parameter string
+        // @todo: parse and replace those parameters where necessary
+        reversedParameters.add(parameters.get(0));
+
+        function.setName("StrfTime");
+        function.setParameters(reversedParameters);
+    }
+  }
+
+  private void rewriteDateTruncFunction(Function function, ExpressionList<?> parameters) {
+    switch (parameters.size()) {
+      case 2:
+        ExpressionList<Expression> reversedParameters = new ExpressionList<>();
+
+        // Date Part "ISOWEEK" exists and is not supported on DuckDB
+        if (parameterWEEK(parameters, 1)) {
+          reversedParameters.add(new StringValue("WEEK"));
+          buffer.append(" /*APPROXIMATION: WEEK*/ ");
+        } else if (parameters.get(1) instanceof Column && ((Column) parameters.get(1)).toString()
+                .replaceAll(" ", "").equalsIgnoreCase("ISOWEEK")) {
+          reversedParameters.add(new StringValue("WEEK"));
+        } else {
+          // translate DAY into String 'DAY'
+          reversedParameters.add(!(parameters.get(1) instanceof StringValue)
+                                 ? new StringValue(parameters.get(1).toString())
+                                 :parameters.get(1));
+        }
+
+        // enforce DATE casting
+        reversedParameters.add(parameters.get(0) instanceof StringValue
+                               ? new DateTimeLiteralExpression().withType(DateTimeLiteralExpression.DateTime.DATE)
+                                       .withValue(((StringValue) parameters.get(0)).toString())
+                               :parameters.get(0));
+
+        function.setParameters(reversedParameters);
+    }
+  }
+
+  private static void rewriteDateSubFunction(Function function, ExpressionList<?> parameters) {
+    switch (parameters.size()) {
+      case 2:
+        if (parameters.get(1) instanceof IntervalExpression) {
+          IntervalExpression interval = (IntervalExpression) parameters.get(1);
+          String negatedParameter =
+                  interval.getParameter().startsWith("-") ? interval.getParameter().substring(1)
+                                                          : "-" + interval.getParameter();
+          interval.setExpression(
+                  new StringValue(negatedParameter + " " + interval.getIntervalType()));
+          interval.setIntervalType("");
+          function.setName("DATE_ADD");
+        }
+    }
+  }
+
+  private void rewriteDateDiffFunction(Function function, ExpressionList<?> parameters) {
+    switch (parameters.size()) {
+      case 3:
+        ExpressionList<Expression> reversedParameters = new ExpressionList<>();
+        // Date Part "ISOWEEK" exists and is not supported on DuckDB
+        if (parameterWEEK(parameters, 2)) {
+          reversedParameters.add(new StringValue("WEEK"));
+          buffer.append(" /*APPROXIMATION: WEEK*/ ");
+        } else if (parameters.get(2) instanceof Column && ((Column) parameters.get(2)).toString()
+                .replaceAll(" ", "").equalsIgnoreCase("ISOWEEK")) {
+          reversedParameters.add(new StringValue("WEEK"));
+        } else {
+          // translate DAY into String 'DAY'
+          reversedParameters.add(!(parameters.get(2) instanceof StringValue)
+                                 ? new StringValue(parameters.get(2).toString())
+                                 :parameters.get(2));
+        }
+
+        // enforce DATE casting
+        reversedParameters.add(parameters.get(1) instanceof StringValue
+                               ? new DateTimeLiteralExpression().withType(DateTimeLiteralExpression.DateTime.DATE)
+                                       .withValue(((StringValue) parameters.get(1)).toString())
+                               :parameters.get(1));
+
+        // enforce DATE casting
+        reversedParameters.add(parameters.get(0) instanceof StringValue
+                               ? new DateTimeLiteralExpression().withType(DateTimeLiteralExpression.DateTime.DATE)
+                                       .withValue(((StringValue) parameters.get(0)).toString())
+                               :parameters.get(0));
+        function.setParameters(reversedParameters);
+    }
+  }
+
+  private static void rewriteDateAddFunction(ExpressionList<?> parameters) {
+    switch (parameters.size()) {
+      case 2:
+        if (parameters.get(1) instanceof IntervalExpression) {
+          IntervalExpression interval = (IntervalExpression) parameters.get(1);
+          interval.setExpression(
+                  new StringValue(interval.getParameter() + " " + interval.getIntervalType()));
+          interval.setIntervalType("");
+        }
+    }
+  }
+
+  private Expression rewriteDateFunction(Function function, ExpressionList<?> parameters) {
+    CastExpression castExpression = null;
+    switch (parameters.size()) {
+      case 1:
+        // DATE(DATETIME '2016-12-25 23:59:59') AS date_dt
+      case 2:
+        // DATE(TIMESTAMP '2016-12-25 05:30:00+07', 'America/Los_Angeles') AS date_tstz
+        buffer.append(" /*APPROXIMATION: timezone not supported*/ ");
+        castExpression = new CastExpression("Cast").withLeftExpression(parameters.get(0))
+                .withType(new ColDataType().withDataType("DATE"));
+        visit(castExpression);
+        break;
+      case 3:
+        function.setName("MAKE_DATE");
+        break;
+    }
+    return castExpression;
+  }
+
+  private void rewriteCurrentDateFunction(ExpressionList<?> parameters) {
+    switch (parameters.size()) {
+      case 1:
+        // CURRENT_DATE(timezone) is not supported in DuckDB
+        buffer.append(" /*APPROXIMATION: timezone not supported*/ ");
+        parameters.clear();
+    }
+  }
+
+  public void visit(ExtractExpression extractExpression) {
+    //@todo: JSQLParser Extract Expression must support `WEEK(MONDAY) .. WEEK(SUNDAY)`
+
+    if (extractExpression.getName().equalsIgnoreCase("WEEK")) {
+      buffer.append(" /*APPROXIMATION: WEEK*/ ");
+      extractExpression.setName("WEEK");
+    } else if (extractExpression.getName().equalsIgnoreCase("ISOWEEK")) {
+      extractExpression.setName("WEEK");
+    }
+
+    if (extractExpression.getExpression() instanceof StringValue) {
+      extractExpression.setExpression(
+              new DateTimeLiteralExpression()
+                      .withType(DateTimeLiteralExpression.DateTime.DATE)
+                      .withValue(extractExpression.toString())
+      );
+    }
+    super.visit(extractExpression);
   }
 }
