cedfc3005eb6b02707397483337692e596bc14f8
manticore-projects <andreas@manticore-projects.com>
2025-06-14 13:32:29 +0700

    style: fix Q/A
    Signed-off-by: Andreas Reichel <andreas@manticore-projects.com>

--- src/test/java/ai/starlake/transpiler/JSQLReplacerTest.java
+++ src/test/java/ai/starlake/transpiler/JSQLReplacerTest.java
@@ -7,107 +7,111 @@ import net.sf.jsqlparser.statement.select.PlainSelect;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import java.sql.SQLException;
 import java.util.Map;
 
 class JSQLReplacerTest {
 
-    @Test
-    void testWithShadowing() throws JSQLParserException {
-        String[][] schemaDefinition = {{"a", "col1", "col2", "col3"}};
+  @Test
+  void testWithShadowing() throws JSQLParserException {
+    String[][] schemaDefinition = {{"a", "col1", "col2", "col3"}};
 
-        // WITH item `a` shadows the base table and does not contain a column `col1`
-        String sqlStr = "with a as (select 1 from a) SELECT a.col1 from a;";
-        JSQLReplacer replacer = new JSQLReplacer(schemaDefinition);
+    // WITH item `a` shadows the base table and does not contain a column `col1`
+    String sqlStr = "with a as (select 1 from a) SELECT a.col1 from a;";
+    JSQLReplacer replacer = new JSQLReplacer(schemaDefinition);
 
-        // So we expect a Column Not Found exception
-        Assertions.assertThatExceptionOfType(ColumnNotFoundException.class).isThrownBy(() -> {
-            PlainSelect st = (PlainSelect) replacer.replace(sqlStr, Map.of("a", "test"));
+    // So we expect a Column Not Found exception
+    Assertions.assertThatExceptionOfType(ColumnNotFoundException.class).isThrownBy(() -> {
+      PlainSelect st = (PlainSelect) replacer.replace(sqlStr, Map.of("a", "test"));
 
-            // only physical BASE TABLE would get replaced
-            Assertions.assertThat(st.toString())
-                      .isEqualToIgnoringCase("with a as (select 1 from test) SELECT a.col1 from a;");
-        });
+      // only physical BASE TABLE would get replaced
+      Assertions.assertThat(st.toString())
+          .isEqualToIgnoringCase("with a as (select 1 from test) SELECT a.col1 from a;");
+    });
 
-        // only physical BASE TABLE would get replaced
-        String sqlStr2 = "with a as (select a.col2 AS col1 from a) SELECT a.col1 from a;";
-        PlainSelect st = (PlainSelect) replacer.replace(sqlStr2, Map.of("a", "test"));
-        Assertions.assertThat(st.toString()).isEqualToIgnoringCase(
-                "with a as (select test.col2 AS col1 from test) SELECT a.col1 from a");
-    }
+    // only physical BASE TABLE would get replaced
+    String sqlStr2 = "with a as (select a.col2 AS col1 from a) SELECT a.col1 from a;";
+    PlainSelect st = (PlainSelect) replacer.replace(sqlStr2, Map.of("a", "test"));
+    Assertions.assertThat(st.toString()).isEqualToIgnoringCase(
+        "with a as (select test.col2 AS col1 from test) SELECT a.col1 from a");
+  }
 
-    @Test
-    void testBaseTableResolution() throws JSQLParserException {
-        String[][] schemaDefinition = {{"a", "col1", "col2", "col3"}};
+  @Test
+  void testBaseTableResolution() throws JSQLParserException {
+    String[][] schemaDefinition = {{"a", "col1", "col2", "col3"}};
 
-        // base table contains a column `col1`
-        String sqlStr = "SELECT a.col1 from a;";
-        JSQLReplacer replacer = new JSQLReplacer(schemaDefinition);
+    // base table contains a column `col1`
+    String sqlStr = "SELECT a.col1 from a;";
+    JSQLReplacer replacer = new JSQLReplacer(schemaDefinition);
 
-        // So we expect no exception
-        Assertions.assertThatNoException().isThrownBy(() -> {
-            PlainSelect st = (PlainSelect) replacer.replace(sqlStr, Map.of("a", "test"));
+    // So we expect no exception
+    Assertions.assertThatNoException().isThrownBy(() -> {
+      PlainSelect st = (PlainSelect) replacer.replace(sqlStr, Map.of("a", "test"));
 
-            // only physical BASE TABLE would get replaced
-            Assertions.assertThat(st.toString()).isEqualToIgnoringCase("SELECT test.col1 from test");
-        });
-    }
+      // only physical BASE TABLE would get replaced
+      Assertions.assertThat(st.toString()).isEqualToIgnoringCase("SELECT test.col1 from test");
+    });
+  }
 
-    @Test
-    void testAliasShadowing() throws JSQLParserException {
-        String[][] schemaDefinition = {{"a", "col1", "col2", "col3"}, {"b", "cola", "colb", "colc"}};
+  @Test
+  void testAliasShadowing() throws JSQLParserException {
+    String[][] schemaDefinition = {{"a", "col1", "col2", "col3"}, {"b", "cola", "colb", "colc"}};
 
-        // base table contains a column `col1`
-        String sqlStr = "SELECT b.col1 from a AS b;";
-        JSQLReplacer resolver = new JSQLReplacer(schemaDefinition);
+    // base table contains a column `col1`
+    String sqlStr = "SELECT b.col1 from a AS b;";
+    JSQLReplacer resolver = new JSQLReplacer(schemaDefinition);
 
-        // So we expect no exception
-        Assertions.assertThatNoException().isThrownBy(() -> {
-            PlainSelect select = (PlainSelect) resolver.replace(sqlStr, Map.of("b", "test"));
+    // So we expect no exception
+    Assertions.assertThatNoException().isThrownBy(() -> {
+      PlainSelect select = (PlainSelect) resolver.replace(sqlStr, Map.of("b", "test"));
 
-            // table column is shadowed by alias, so table must not get resolved
-            Column c = select.getSelectItem(0).getExpression(Column.class);
-            Assertions.assertThat(c.getResolvedTable()).isNull();
-        });
-    }
+      // table column is shadowed by alias, so table must not get resolved
+      Column c = select.getSelectItem(0).getExpression(Column.class);
+      Assertions.assertThat(c.getResolvedTable()).isNull();
+    });
+  }
 
-    @Test
-    void testSwapTablenames() throws JSQLParserException {
-        String sqlStr = "SELECT a.*\n"
-                        + "FROM (  SELECT  a.col3\n"
-                        + "                , Sum( a.col2 )\n"
-                        + "        FROM a inner join b on a.col1=b.col1\n"
-                        + "        WHERE a.col1 = b.col1\n"
-                        + "        GROUP BY a.col3\n"
-                        + "        HAVING Sum( a.col2 ) > 0 ) AS a";
+  @Test
+  void testSwapTablenames() throws JSQLParserException {
+    // @formatter:off
+    String sqlStr =
+        "SELECT a.*\n"
+        + "FROM (  SELECT  a.col3\n"
+        + "                , Sum( a.col2 )\n"
+        + "        FROM a inner join b on a.col1=b.col1\n"
+        + "        WHERE a.col1 = b.col1\n"
+        + "        GROUP BY a.col3\n"
+        + "        HAVING Sum( a.col2 ) > 0 ) AS a";
+    // @formatter:on
 
-        Map<String, String> replacements =  Map.of("a", "b", "b", "a");
+    Map<String, String> replacements = Map.of("a", "b", "b", "a");
 
-        String expected = "SELECT a.col3, a.sum \n"
-                          + "FROM (  SELECT  b.col3\n"
-                          + "                , Sum( b.col2 )\n"
-                          + "        FROM b inner join a on b.col1=a.col1\n"
-                          + "        WHERE b.col1 = a.col1\n"
-                          + "        GROUP BY b.col3\n"
-                          + "        HAVING Sum( b.col2 ) > 0 ) AS a";
+    // @formatter:off
+    String expected =
+        "SELECT a.col3, a.sum \n"
+        + "FROM (  SELECT  b.col3\n"
+        + "                , Sum( b.col2 )\n"
+        + "        FROM b inner join a on b.col1=a.col1\n"
+        + "        WHERE b.col1 = a.col1\n"
+        + "        GROUP BY b.col3\n"
+        + "        HAVING Sum( b.col2 ) > 0 ) AS a";
+    // @formatter:on
 
-        assertThatRewritesInto(sqlStr, replacements, expected);
-    }
+    assertThatRewritesInto(sqlStr, replacements, expected);
+  }
 
-    Statement assertThatRewritesInto(String sqlStr, Map<String, String> replacements, String expected)
-            throws JSQLParserException {
-        String[][] schemaDefinition = {{"a", "col1", "col2", "col3"}, {"b", "col1", "col2", "col3"},
-                {"test", "col1", "col2", "col3"}};
-        return assertThatRewritesInto(schemaDefinition, sqlStr, replacements, expected);
-    }
+  Statement assertThatRewritesInto(String sqlStr, Map<String, String> replacements, String expected)
+      throws JSQLParserException {
+    String[][] schemaDefinition = {{"a", "col1", "col2", "col3"}, {"b", "col1", "col2", "col3"},
+        {"test", "col1", "col2", "col3"}};
+    return assertThatRewritesInto(schemaDefinition, sqlStr, replacements, expected);
+  }
 
-    Statement assertThatRewritesInto(String[][] schemaDefinition, String sqlStr, Map<String, String> replacements, String expected)
-            throws JSQLParserException {
-        JSQLReplacer replacer = new JSQLReplacer(schemaDefinition);
-        Statement st = replacer.replace(sqlStr, replacements);
-        Assertions
-                .assertThat(JSQLColumnResolverTest.sanitize(st.toString()))
-                .isEqualToIgnoringCase(JSQLColumnResolverTest.sanitize(expected));
-        return st;
-    }
+  Statement assertThatRewritesInto(String[][] schemaDefinition, String sqlStr,
+      Map<String, String> replacements, String expected) throws JSQLParserException {
+    JSQLReplacer replacer = new JSQLReplacer(schemaDefinition);
+    Statement st = replacer.replace(sqlStr, replacements);
+    Assertions.assertThat(JSQLColumnResolverTest.sanitize(st.toString()))
+        .isEqualToIgnoringCase(JSQLColumnResolverTest.sanitize(expected));
+    return st;
+  }
 }
